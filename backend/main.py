3ce595111a792707d5ee29e19e0918a75f8aeda7
Gabriel Sousa <gabriel.gs@sidi.org.br>
2024-10-18 17:15:23 -0300

    feat: started the backend using python + fastapi

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from pymongo_get_database import get_database
+from test_insert import instert_via_api
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    result =instert_via_api()
+    print(result)
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: str):
+    db = get_database()
+    collection = db["user_1_items"]
+    item = collection.find({"_id": item_id})
+    return {"item": item[0]}
